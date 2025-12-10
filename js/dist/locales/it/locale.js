5ba417c7a6ac1933b518430400da2f69b51705c3
UO-TransizioneDigitaleComunePalermo <42996217+UO-TransizioneDigitaleComunePalermo@users.noreply.github.com>
2025-12-10 16:53:11 +0100

    Update Italian locale labels for item types

--- js/dist/locales/it/locale.js
+++ js/dist/locales/it/locale.js
@@ -66,8 +66,8 @@ Exhibit.Localization.importLocale("it", {
     "%import.failedAccessHttpStatus": " (HTTP %1$d)",
     "%import.missingFragment": "Failed to locate fragment in %1$s",
 
-    "%database.itemType.label": "documento",
-    "%database.itemType.pluralLabel": "documenti",
+    "%database.itemType.label": "UO",
+    "%database.itemType.pluralLabel": "UO",
     "%database.labelProperty.label": "label",
     "%database.labelProperty.pluralLabel": "labels",
     "%database.labelProperty.reverseLabel": "label of",
